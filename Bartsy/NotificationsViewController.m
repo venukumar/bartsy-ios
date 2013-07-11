5b4f8c70e64d25414fd32c67a04e5916bffa8adc
swathik <swathik@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-11 13:55:52 +0000

    Completed Notifications UI
    git-svn-id: http://192.168.2.90/svn/bartsy-ios/bartsy-ios@154 c3b44e55-a100-42f6-9c86-32dbf579054b

--- Bartsy/NotificationsViewController.m
+++ Bartsy/NotificationsViewController.m
@@ -28,15 +28,31 @@
 {
     [super viewDidLoad];
     //self.title = @"Notifications";
-    self.navigationController.navigationBarHidden=NO;
+    self.navigationController.navigationBarHidden = YES;
     self.sharedController=[SharedController sharedController];
     
     arrayForNotifications = [[NSMutableArray alloc] init];
-    UITableView *tblView=[[UITableView alloc]initWithFrame:CGRectMake(0, 0, 320, 415)];
+        
+    UIImageView *imgViewForTop = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    imgViewForTop.image=[UIImage imageNamed:@"top_header_bar.png"];
+    [self.view addSubview:imgViewForTop];
+    [imgViewForTop release];
+    
+    UIImageView *imgLogo = [[UIImageView alloc] initWithFrame:CGRectMake(100.25, 13.25, 119.5, 23.5)];
+    imgLogo.image=[UIImage imageNamed:@"logo_Header.png"];
+    [self.view addSubview:imgLogo];
+    [imgLogo release];
+    
+    UIButton *btnSearch = [self createUIButtonWithTitle:@"" image:[UIImage imageNamed:@"search_icon.png"] frame:CGRectMake(10, 14.5, 22, 21) tag:0 selector:@selector(btnSearch_TouchUpInside:) target:self];
+    [self.view addSubview:btnSearch];
+
+    UITableView *tblView=[[UITableView alloc]initWithFrame:CGRectMake(0, 45, 320, 415)];
     if (IS_IPHONE_5)
     {
-        tblView.frame = CGRectMake(0, 0, 320, 495);
+        tblView.frame = CGRectMake(0, 45, 320, 455);
     }
+    tblView.tableFooterView = [[[UIView alloc] initWithFrame:CGRectZero] autorelease];
+    tblView.backgroundColor = [UIColor blackColor];
     tblView.dataSource=self;
     tblView.delegate=self;
     tblView.tag=111;
@@ -66,6 +82,11 @@
 {
     UITableViewCell *cell =[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
     
+    if((indexPath.row)==0)
+        cell.contentView.backgroundColor = [UIColor colorWithPatternImage:[UIImage            imageNamed:@"city_tavern_bg.png"]]; //set image for cell 0
+    else
+        cell.contentView.backgroundColor = [UIColor colorWithPatternImage:[UIImage            imageNamed:@"fathers_office-bg.png"]];
+    
     if ([arrayForNotifications count])
     {
         if ([[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"type"]isEqualToString:@"checkin"]||[[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"type"]isEqualToString:@"checkout"]||[[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"orderType"]isEqualToString:@"self"])
@@ -84,14 +105,14 @@
             [cell.contentView addSubview:lblVenueName];
             //[lblVenueName release];
             
-            UILabel *lblItemName = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"message"] frame:CGRectMake(61,25,250,40) tag:0 font:[UIFont boldSystemFontOfSize:13] color:[UIColor blackColor] numberOfLines:2];
+            UILabel *lblItemName = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"message"] frame:CGRectMake(61,25,250,40) tag:0 font:[UIFont boldSystemFontOfSize:13] color:[UIColor colorWithRed:204.0/225.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0] numberOfLines:2];
             lblItemName.backgroundColor=[UIColor clearColor];
             lblItemName.textAlignment = NSTextAlignmentLeft;
             [cell.contentView addSubview:lblItemName];
             //[lblItemName release];
             
             
-            UILabel *lblDate = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"createdTime"] frame:CGRectMake(61,65,250,20) tag:0 font:[UIFont boldSystemFontOfSize:13] color:[UIColor blackColor] numberOfLines:1];
+            UILabel *lblDate = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"createdTime"] frame:CGRectMake(61,65,250,20) tag:0 font:[UIFont boldSystemFontOfSize:13] color:[UIColor colorWithRed:204.0/225.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0] numberOfLines:1];
             lblDate.backgroundColor=[UIColor clearColor];
             lblDate.textAlignment = NSTextAlignmentLeft;
             [cell.contentView addSubview:lblDate];
@@ -109,20 +130,20 @@
             [[cell.imageView layer] setShadowRadius:3.0];
             [[cell.imageView layer] setShadowOpacity:0.8];
             
-            UILabel *lblVenueName = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"venueName"] frame:CGRectMake(61, 5, 200, 20) tag:0 font:[UIFont boldSystemFontOfSize:15] color:[UIColor grayColor] numberOfLines:1];
+            UILabel *lblVenueName = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"venueName"] frame:CGRectMake(61, 5, 200, 20) tag:0 font:[UIFont boldSystemFontOfSize:15] color:[UIColor colorWithRed:204.0/225.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0] numberOfLines:1];
             lblVenueName.text=@"VenueName";
             lblVenueName.backgroundColor=[UIColor clearColor];
             lblVenueName.textAlignment = NSTextAlignmentLeft;
             [cell.contentView addSubview:lblVenueName];
             //[lblVenueName release];
             
-            UILabel *lblItemName = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"message"] frame:CGRectMake(61,25,200,40) tag:0 font:[UIFont boldSystemFontOfSize:13] color:[UIColor blackColor] numberOfLines:2];
+            UILabel *lblItemName = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"message"] frame:CGRectMake(61,25,200,40) tag:0 font:[UIFont boldSystemFontOfSize:13] color:[UIColor colorWithRed:204.0/225.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0] numberOfLines:2];
             lblItemName.backgroundColor=[UIColor clearColor];
             lblItemName.textAlignment = NSTextAlignmentLeft;
             [cell.contentView addSubview:lblItemName];
             //[lblItemName release];
             
-            UILabel *lblDate = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"createdTime"] frame:CGRectMake(61,65,200,20) tag:0 font:[UIFont boldSystemFontOfSize:13] color:[UIColor blackColor] numberOfLines:1];
+            UILabel *lblDate = [self createLabelWithTitle:[[arrayForNotifications objectAtIndex:indexPath.row] objectForKey:@"createdTime"] frame:CGRectMake(61,65,200,20) tag:0 font:[UIFont boldSystemFontOfSize:13] color:[UIColor colorWithRed:204.0/225.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0] numberOfLines:1];
             lblDate.backgroundColor=[UIColor clearColor];
             lblDate.textAlignment = NSTextAlignmentLeft;
             [cell.contentView addSubview:lblDate];
@@ -143,6 +164,9 @@
     else
     {
         cell.textLabel.text=@"No Notifications";
+        cell.textLabel.backgroundColor = [UIColor clearColor];
+        cell.textLabel.textAlignment = NSTextAlignmentCenter;
+        cell.textLabel.textColor = [UIColor colorWithRed:204.0/225.0 green:204.0/255.0 blue:204.0/255.0 alpha:1.0];
     }
     return cell;
 
@@ -174,6 +198,7 @@
     else if ([[result objectForKey:@"notifications"] count])
     {
         [arrayForNotifications addObjectsFromArray:[result objectForKey:@"notifications"]];
+        NSLog(@"notifications is %@",arrayForNotifications);
         UITableView *tblView=(UITableView*)[self.view viewWithTag:111];
         [tblView reloadData];
         [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(reloadTable) userInfo:nil repeats:NO];
