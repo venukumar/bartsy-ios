be8bec6b7f43796f67fa0231f03c9da224cd7240
sudheerp <sudheerp@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-08-30 06:22:45 +0000

    Past Orders View Added
    git-svn-id: http://192.168.2.90/svn/bartsy-ios/bartsy-ios@300 c3b44e55-a100-42f6-9c86-32dbf579054b

---
+++ Bartsy/PastOrderView.h
@@ -0,0 +1,13 @@
+//
+//  PastOrderView.h
+//  Bartsy
+//
+//  Created by Techvedika on 8/30/13.
+//  Copyright (c) 2013 TechVedika Software Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PastOrderView : UIViewController
+-(IBAction)btnBack_TouchUpInside;
+@end
