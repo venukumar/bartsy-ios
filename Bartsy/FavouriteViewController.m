b7f0b40c41ff2c7831dfda4512dc2e80d8f6cbde
sudheerp <sudheerp@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-08-30 06:23:30 +0000

    Favorite View added
    git-svn-id: http://192.168.2.90/svn/bartsy-ios/bartsy-ios@301 c3b44e55-a100-42f6-9c86-32dbf579054b

---
+++ Bartsy/FavouriteViewController.m
@@ -0,0 +1,43 @@
+//
+//  FavouriteViewController.m
+//  Bartsy
+//
+//  Created by Techvedika on 8/30/13.
+//  Copyright (c) 2013 TechVedika Software Pvt Ltd. All rights reserved.
+//
+
+#import "FavouriteViewController.h"
+
+@interface FavouriteViewController ()
+
+@end
+
+@implementation FavouriteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+//Back button action
+-(IBAction)btnBack_TouchUpInside
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
